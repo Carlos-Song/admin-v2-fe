09bfd68e7b1f71f16ccfd17c19899cfb7abc7f77
silverwing <silverwing@icloud.com>
2018-07-03 17:34:25 +0800

    Initial

---
+++ webpack.config.js
@@ -0,0 +1,90 @@
+const path = require('path');
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    entry: './src/app.jsx',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/dist/',
+        filename: 'js/app.js'
+    },
+    module: {
+        rules: [
+            // react 语法的处理
+            {
+                test: /\.jsx$/,
+                exclude: /(node_modules)/,
+                use:{
+                    loader: 'babel-loader',
+                    options:{
+                        presets: ['env','react']
+                    }
+                }
+            },
+
+            // css 文件的处理
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: "css-loader"
+                })
+            },
+
+            // SASS 文件的处理
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'sass-loader']
+                })
+            },
+
+            //图片处理
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 8192,
+                            name:'resource/[name].[ext]'
+                        }
+                    }
+                ]
+            },
+
+            //字体图标的设置
+            {
+                test: /\.(ttf|svg|eot|woff2|woff|otf)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 8192,
+                            name:'resource/[name].[ext]'
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins:[
+        //处理HTML文件
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        // 独立css文件
+        new ExtractTextPlugin('css/[name].css'),
+        // 提出公共模块
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'common',
+            filename: 'js/base.js'
+        })
+    ],
+    devServer: {
+        port: 8086
+    }
+};
